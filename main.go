5ca63dd6fcf7dec2b7b43f052d6e4df8f11b4fb9
louna <r.arswendo.r@gmail.com>
2024-10-05 20:28:50 +0700

    todo: added notes

--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("welcome to ngram")
+	// TODO: Make a hashing utility that implement either the Linear Probing or Chaining Collision Detections
+	// NOTE: Using the result of the (ASCII Char % size of map) is a nice way to key it using integer
 }
