e323e7bc1f188747cecc863039c00f4bb34d5688
louna <r.arswendo.r@gmail.com>
2024-10-06 16:11:49 +0700

    refactor: changed collision detection from chaining to linear probing

--- main.go
+++ main.go
@@ -1,9 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("welcome to ngram")
-	// TODO: Make a hashing utility that implement either the Linear Probing or Chaining Collision Detections
-	// NOTE: Using the result of the (ASCII Char % size of map) is a nice way to key it using integer
 }
